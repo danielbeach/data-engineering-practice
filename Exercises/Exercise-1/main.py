21e1ae5a31160ed04a2353637c9f68cf3b354cfc
Mide clp <aogunnola@gmail.com>
2022-04-02 13:32:20 +0100

    Completed exercise

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,9 @@
 import requests
+from pathlib import Path
+import os
+import concurrent.futures
+import zipfile
+import glob
 
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
@@ -11,9 +16,69 @@ download_uris = [
 ]
 
 
+# create the downloads directory
+def create_directory():
+    if "Downloads" not in os.listdir():
+        p = Path("Downloads/")
+        p.mkdir()
+
+
+# download the zip file
+def download_files():
+    if len(os.listdir("Downloads")) < 7:
+        try:
+            os.removedirs("Downloads")
+        except OSError as e:
+            print("directory does not exist")
+            create_directory()
+        else:
+            create_directory()
+
+        def download_zip(download_uri):
+
+            zip_name = download_uri.split("/")[-1]
+            response = requests.get(download_uri)
+            with open(f"Downloads/{zip_name}", "wb") as f:
+                f.write(response.content)
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            executor.map(download_zip, download_uris)
+    else:
+        print("Files already exist")
+
+
+# extract each files
+def extract_file():
+    zip_files = os.listdir("Downloads/")
+    if len(zip_files) >= 6:
+        def extract(file):
+            d_zip = zipfile.ZipFile(f"Downloads/{file}", "r")
+            d_zip.extractall(path="Downloads/")
+            d_zip.close()
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            executor.map(extract, zip_files)
+
+
+# delete the extracted zip file
+def drop_zipfiles():
+    for zip_file in glob.glob("Downloads/*.zip"):
+        os.remove(zip_file)
+        # print(zip_file)
+
+
 def main():
-    # your code here
-    pass
+    # create directory
+    create_directory()
+
+    # download files
+    download_files()
+
+    # extract the files
+    extract_file()
+
+    # delete zip files after extraction
+    drop_zipfiles()
 
 
 if __name__ == '__main__':
