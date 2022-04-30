e652abdd1868243e520d5f4696130fb8639a9d4e
Dongjin Lee <djlee1509@gmail.com>
2022-04-30 22:25:28 +0100

    Add Exercise 1 solution to practice downloading files with Python

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,7 @@
 import requests
+import os
+from zipfile import ZipFile
+
 
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
@@ -13,7 +16,35 @@ download_uris = [
 
 def main():
     # your code here
-    pass
+    for uri in download_uris:
+        response = requests.get(uri, stream=True)
+
+        if response.ok:
+            zip_folder = uri.split('/')[-1]
+            # file_name = os.path.splitext(zip_folder)[0]
+
+            current_directory = os.getcwd()
+            download_dir = 'downloads'
+            dest_path = os.path.join(current_directory, download_dir)
+            zip_path = os.path.join(dest_path, zip_folder)
+            
+            if not os.path.exists(dest_path):
+                os.mkdir(dest_path)
+
+            with open(zip_path, 'wb') as output_file:
+                output_file.write(response.content)
+            print(f"Download completed: {zip_folder}")
+
+            with ZipFile(zip_path, 'r') as zip_file:
+                zip_file.extractall(dest_path)
+                os.remove(zip_path)
+            print(f"Finished extracting zip file: {zip_folder}")
+
+            rubbish_dir = os.path.join(dest_path, '__MACOSX')
+            if os.path.exists(rubbish_dir):
+                os.rmdir(rubbish_dir)
+        else:
+            print(f"This URL is not valid: {uri}")
 
 
 if __name__ == '__main__':
