1e98b3d1dbe4c12f01454dfef0a76f01894230f7
Yoram <52070451+YFrenkiel@users.noreply.github.com>
2023-04-13 11:58:53 +0200

    Add Exercise 1

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,6 @@
 import requests
+import zipfile
+import os
 
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
@@ -12,9 +14,28 @@ download_uris = [
 
 
 def main():
-    # your code here
-    pass
 
+    PATH = "Exercise-1/downloads"
+
+    try:
+        os.makedirs(PATH)
+    except FileExistsError:
+        pass
+
+    for uri in download_uris:
+        filename = uri.split('/')[-1]        
+        
+        r = requests.get(uri, allow_redirects=True)
+        if r.status_code == 200:
+            open(f"{PATH}/{filename}", 'wb').write(r.content)
+    
+    for zip in os.listdir(PATH):
+        with zipfile.ZipFile(f"{PATH}/{zip}") as zip_ref:
+            zip_ref.extractall(PATH)
+
+    for file in os.listdir(PATH):
+        if file.endswith(".zip"):
+            os.remove(f"{PATH}/{file}")
 
 if __name__ == '__main__':
     main()
