32b3124d1b095ffb95a4f368c1e20a391ddd299e
Polina <sakakawea666@gmail.com>
2023-09-29 22:12:37 +0200

    feat: parallel downloading

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,6 +1,8 @@
 import requests
 import zipfile
 import os
+import tempfile
+import concurrent.futures
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -14,19 +16,28 @@ download_uris = [
 
 
 def main():
-    for uri in download_uris:
-        download_and_unzip(uri)
+    max_threads = 4
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+        futures = [executor.submit(download_and_unzip, uri) for uri in download_uris]
+
+        concurrent.futures.wait(futures)
 
 
 def download_and_unzip(uri: str, to='downloads'):
     os.makedirs(to, exist_ok=True)
-    r = requests.get(uri)
-    if r:
-        with open('test.zip', 'wb') as f:
-            f.write(r.content)
-        with zipfile.ZipFile('test.zip', 'r') as zip_ref:
-            zip_ref.extractall(to)
-        os.remove('test.zip')
+    temp_dir = tempfile.mkdtemp()
+    try:
+        r = requests.get(uri)
+        if r.status_code == 200:
+            filename = os.path.join(temp_dir, os.path.basename(uri))
+            with open(filename, 'wb') as file:
+                file.write(r.content)
+            with zipfile.ZipFile(filename, 'r') as zip_ref:
+                zip_ref.extractall(to)
+        else:
+            print(f"Failed to download file from {uri}. Status code: {r.status_code}")
+    except Exception as e:
+        print(f"An error occurred while downloading {uri}: {str(e)}")
 
 
 if __name__ == "__main__":
