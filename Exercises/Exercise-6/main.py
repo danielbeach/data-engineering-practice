cbcbc96545029eb20e83fe56d53c41eeadb1d413
LolindaLP <sakakawea666@gmail.com>
2023-10-29 00:27:03 +0200

    last fixes

--- Exercises/Exercise-6/main.py
+++ Exercises/Exercise-6/main.py
@@ -1,14 +1,19 @@
-from pyspark import rdd
-from pyspark.shell import spark
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import to_date, col, unix_timestamp, concat, lit
+from datetime import datetime, timedelta
+from pyspark.sql import functions as F
+from pyspark.sql import SparkSession, Window
+from pyspark.sql.functions import to_date, col, unix_timestamp, month, rank, count, row_number, from_unixtime, avg, \
+    to_timestamp, date_format
 import zipfile
 import os
 import tempfile
 
 
 def main():
-    avg_trip_duration_per_day(create_data_frame())
+    # avg_trip_duration_per_day(create_data_frame())
+    # trips_per_day(create_data_frame())
+    # popular_month_station(create_data_frame())
+    # top_station_by_day(create_data_frame())
+    longest_trips(create_data_frame())
 
 
 def create_data_frame():
@@ -33,7 +38,6 @@ def create_data_frame():
 
 def avg_trip_duration_per_day(dataframes):
     combined_df = None
-    output_path = "reports/analysis_1"
     for df in dataframes:
         if "start_time" in df.columns:
             df = df.withColumn("day", to_date(col("start_time")))
@@ -51,17 +55,131 @@ def avg_trip_duration_per_day(dataframes):
 
     not_sorted_result = combined_df.groupBy("day").agg({"trip_duration": "avg"})
     result = not_sorted_result.orderBy("day")
-    result.coalesce(1).write.option("header", "true").csv(output_path)
 
-    files = os.listdir(output_path)
-    for file in files:
-        if file.endswith(".csv"):
-            csv_file_path = os.path.join(output_path, file)
-            new_csv_file_path = os.path.join(output_path, "average_trip_duration_per_day.csv")
-            os.rename(csv_file_path, new_csv_file_path)
+    output_result(result, "reports/analysis_1", "trip_duration.csv")
+
+
+def trips_per_day(dataframes):
+    combined_df = None
+    for df in dataframes:
+        if "started_at" in df.columns:
+            df = df.withColumn("date", to_date(col("started_at")))
+        elif "start_time" in df.columns:
+            df = df.withColumn("date", to_date(col("start_time")))
+        common_columns = ["date"]
+        df = df.select(common_columns)
+
+        if combined_df is None:
+            combined_df = df
         else:
-            file_to_delete = os.path.join(output_path, file)
-            os.remove(file_to_delete)
+            combined_df = combined_df.union(df)
+
+    not_sorted_result = combined_df.groupBy("date").count().withColumnRenamed("count", "trip_count")
+    result = not_sorted_result.orderBy("date")
+
+    output_result(result, "reports/analysis_2", "trips_per_day.csv")
+
+
+def popular_month_station(dataframes):
+    station_column = None
+    combined_df = None
+    for df in dataframes:
+        if "started_at" in df.columns:
+            df = df.withColumn("month", month(col("started_at")))
+            station_column = "start_station_name"
+        elif "start_time" in df.columns:
+            df = df.withColumn("month", month(col("start_time")))
+            station_column = "from_station_name"
+
+        if station_column:
+            df = df.groupBy("month", station_column).agg({"*": "count"})
+            df = df.withColumnRenamed("count(1)", "trip_count")
+
+            if combined_df is None:
+                combined_df = df
+            else:
+                combined_df = combined_df.union(df)
+
+    window_spec = Window.partitionBy("month").orderBy(col("trip_count").desc())
+
+    popular_station_by_month = combined_df.withColumn("rank", rank().over(window_spec))
+
+    popular_station_by_month = popular_station_by_month.filter(col("rank") == 1)
+
+    result = popular_station_by_month.orderBy("month")
+
+    output_result(result, "reports/analysis_3", "popular_station_per_month.csv")
+
+
+def top_station_by_day(dataframes):
+    combined_df = None
+    for df in dataframes:
+        if "start_time" in df.columns:
+            df = df.withColumnRenamed("start_time", "started_at")
+            df = df.withColumnRenamed("from_station_name", "start_station_name")
+
+        df = filter_last_two_weeks(df, "started_at")
+        df = df.select("started_at", "start_station_name")
+        df = df.withColumn("started_at", to_date(df["started_at"]))
+        df = df.groupBy("started_at", "start_station_name").agg(count("*").alias("trip_count"))
+        windowSpec = Window.partitionBy("started_at").orderBy(F.desc("trip_count"))
+        ranked_df = df.withColumn("station_rank", row_number().over(windowSpec))
+        df = ranked_df.filter(F.col("station_rank") <= 3)
+
+        if combined_df is None:
+            combined_df = df
+        else:
+            combined_df = combined_df.union(df)
+
+    result = combined_df.orderBy("started_at")
+    output_result(result, "reports/analysis_4", "top_station_by_day.csv")
+
+
+def longest_trips(dataframes):
+    for df in dataframes:
+        try:
+            df = df.select("start_time", "end_time", "gender")
+            df = df.withColumn("trip_duration", (unix_timestamp("end_time") - unix_timestamp("start_time")))
+            average_trip_duration = df.groupBy("gender").agg(avg("trip_duration"))
+        except Exception as e:
+            print(e)
+            continue
+
+        output_result(average_trip_duration, "reports/analysis_5", "longest_trips.csv")
+
+
+def output_result(result, output_path, name):
+    try:
+        result.coalesce(1).write.option("header", "true").csv(output_path)
+
+        files = os.listdir(output_path)
+        for file in files:
+            if file.endswith(".csv"):
+                csv_file_path = os.path.join(output_path, file)
+                new_csv_file_path = os.path.join(output_path, name)
+                os.rename(csv_file_path, new_csv_file_path)
+            else:
+                file_to_delete = os.path.join(output_path, file)
+                os.remove(file_to_delete)
+
+    except Exception as e:
+        if "exists" in str(e):
+            print(f"!!!File {name} already exists!!!")
+        else:
+            print(e)
+
+
+def filter_last_two_weeks(df, start):
+    try:
+        max_date = df.selectExpr(f"max({start})").collect()[0][0]
+        max_datetime = datetime.strptime(max_date, "%Y-%m-%d %H:%M:%S")
+        end_date = max_datetime
+        start_date = max_datetime - timedelta(days=14)
+        filtered_data = df.filter((col(start) >= start_date) & (col(start) <= end_date))
+        return filtered_data
+    except Exception as e:
+        print(e)
+        return df
 
 
 if __name__ == "__main__":
