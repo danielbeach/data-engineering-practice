fc7fec168f4674bab146b36532aef34d771fc397
Rajeshwar <rajeshwarrao055@gmail.com>
2023-11-11 21:48:48 +0530

    Adding naive implementation for Exercise 1

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,6 @@
 import requests
+import zipfile
+import os
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -12,9 +14,24 @@ download_uris = [
 
 
 def main():
-    # your code here
-    pass
+    for uri in download_uris:
+        process_uri(uri)
 
+def process_uri(uri):
+    try:
+        filename = os.path.basename(uri)
+        response = requests.get(uri)
+        print('processing file : ' + filename)
+        if response.status_code == 200:
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+                with zipfile.ZipFile(filename, 'r') as zip_ref:
+                    zip_ref.extractall('downloads')
+            os.remove(filename)
+        else:
+            print("Failed downloading zip file")
+    except Exception as e:
+        print(f"Encountered exception: {str(e)}")
 
 if __name__ == "__main__":
     main()
