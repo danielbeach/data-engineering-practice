53a6133ccc562d7d6584ab1ebb052503ff982acc
RuslanAbdullaev <ruslan.abdullaev060994@gmail.com>
2024-06-04 00:50:46 +0300

    Add scipts

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,8 @@
-import requests
+import asyncio
+import os
+import zipfile
+
+import aiohttp
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -11,10 +15,38 @@ download_uris = [
 ]
 
 
-def main():
-    # your code here
-    pass
+def check_directory(dir_name):
+    if not os.path.exists("downloads"):
+        os.makedirs("downloads")
+    os.chdir(dir_name)
+
+
+async def downloads_file(urls):
+    connector = aiohttp.TCPConnector(limit=50, force_close=True)
+    async with aiohttp.ClientSession(connector=connector) as session:
+        for url in urls:
+            filename = os.path.basename(url)
+            async with session.get(url, ssl=False) as response:
+                if response.status == 200:
+                    with open(filename, "wb") as file:
+                        file.write(await response.read())
+                else:
+                    print(f"Download failed: {os.path.basename(url)}")
+
+
+async def unzip():
+    for root, dirs, files in os.walk(os.getcwd()):
+        for file in files:
+            with zipfile.ZipFile(file, "r") as zip_file:
+                zip_file.extractall()
+                os.remove(file)
+
+
+async def main():
+    check_directory("downloads")
+    await downloads_file(download_uris)
+    await unzip()
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
