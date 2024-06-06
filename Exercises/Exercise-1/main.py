1fc6c3fff8de7eee4e6f9c268bbe32da531062f7
fabianhogger <draizfabian@gmail.com>
2024-06-06 22:16:32 +0200

    ex1

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,5 +1,4 @@
 import requests
-
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
@@ -12,9 +11,16 @@ download_uris = [
 
 
 def main():
-    # your code here
-    pass
-
+    for uri in download_uris:
+        #filename=re.findall("\/[a-zA-Z0-9_]+\.zip",uri)[0]
+        #filename=re.sub("/","Downloads/",filename)
+        filename="Downloads"+uri[39:]
+        print(filename)
+        response=requests.get(uri,filename,stream=True)
+        handle = open(filename, "wb")
+        for chunk in response.iter_content(chunk_size=512):
+            if chunk:  # filter out keep-alive new chunks
+                handle.write(chunk)
 
 if __name__ == "__main__":
     main()
