77133acb0c13c56503af6f3d89daf8ec532b870f
rcDeveloping <rcflorestal@yahoo.com.br>
2024-10-07 14:29:22 -0300

    Initial commit

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,8 @@
 import requests
+import os
+import zipfile
+import shutil
+from urllib.request import urlretrieve
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -11,9 +15,64 @@ download_uris = [
 ]
 
 
+def check_url_validity(url):
+    """Check if the URL is valid by sending a Head request."""
+    try:
+        response = requests.head(url, allow_redirects=True)
+        if response.status_code == 200:
+            return True
+        else:
+            str_log = f"URL not valid: {url} (Status Code: {response.status_code})"
+            with open("log_file.txt", "a") as f:
+                f.write(str_log + "\n")
+            print(str_log)
+            return False
+    except Exception as e:
+        print(f"Error checking URL: {url} - {e}")
+        return False
+
+
+def download_and_unzip(url, file_name, download_dir):
+    """Download and unzip files from URLs"""
+    # Save zip file
+    urlretrieve(url, f"{download_dir}/{file_name}")
+
+    # unzip the file
+    with zipfile.ZipFile(f"{download_dir}/{file_name}", "r") as zip_ref:
+        zip_ref.extractall(download_dir)
+
+    # Delete the zip file after the extraction
+    os.remove(f"{download_dir}/{file_name}")
+    # Remove the __MACOSX directory if it exists
+    remove_macosx_dir(download_dir)
+
+
+def remove_macosx_dir(directory):
+    """Remove all __MACOSX directories and their contents recursively"""
+    for root, dirs, files in os.walk(directory):
+        if "__MACOSX" in dirs:
+            macosx_path = os.path.join(root, "__MACOSX")
+            print("Removing {macosx_path} directory...")
+            shutil.rmtree(macosx_path)
+            print("Removed {macosx_path}")
+
+
 def main():
-    # your code here
-    pass
+    # Ensure the 'Downloads' directory exists
+    if not os.path.exists("Downloads"):
+        os.makedirs("Downloads")
+
+    download_dir = "Downloads"
+
+    # List of files names
+    list_files = [i.split("/")[-1] for i in download_uris]
+
+    # Download data files
+    for url, file_name in zip(download_uris, list_files):
+        if check_url_validity(url):
+            download_and_unzip(url, file_name, download_dir)
+        else:
+            print(f"Skipping download for {file_name} due invalid URL.")
 
 
 if __name__ == "__main__":
