d8c0b887e9b721bf738ec13becd80e80f2dc368d
lucvykiet <lucvykiet@gmail.com>
2025-04-24 15:31:44 +0700

    Add files via upload
    them bai 3

--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -1,10 +1,22 @@
 import boto3
+import gzip
+import io
 
+bucket_name = "commoncrawl"
+file_key = "crawl-data/CC-MAIN-2022-05/wet.paths.gz"
 
-def main():
-    # your code here
-    pass
+s3 = boto3.client("s3")
 
+# Tải và đọc file .gz trong RAM
+response = s3.get_object(Bucket=bucket_name, Key=file_key)
+compressed_data = response["Body"].read()
+with gzip.GzipFile(fileobj=io.BytesIO(compressed_data)) as gz:
+    first_line = gz.readline().decode("utf-8").strip()
 
-if __name__ == "__main__":
-    main()
+# Tải file WET từ dòng đầu tiên
+response2 = s3.get_object(Bucket=bucket_name, Key=first_line)
+
+# Đọc từng dòng và in ra
+with gzip.GzipFile(fileobj=response2["Body"]) as f:
+    for line in f:
+        print(line.decode("utf-8").strip())
