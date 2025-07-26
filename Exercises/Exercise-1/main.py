b1081bbc468b9247f819bfbbb05e232c1291a4b0
FCardellino <fndcardellino@gmail.com>
2025-07-26 15:14:39 -0300

    first commit with resolved exercise

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,8 @@
 import requests
+import sys 
+from pathlib import Path
+import zipfile 
+import os
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -13,7 +17,23 @@ download_uris = [
 
 def main():
     # your code here
-    pass
+    #pass
+    cwd = Path.cwd()
+    folder_path = cwd / "downloads" 
+    folder_path.mkdir(parents=True,exist_ok=True)
+
+    for uri in download_uris:
+       
+       file_name = uri.split('/')[-1]
+       file_path = folder_path / file_name
+       response = requests.get(uri)
+       if response.status_code==200:
+        with open(file_path,"wb") as f:
+            f.write(response.content)
+        csv_file = file_name.split(".")[0] + ".csv"
+        with zipfile.ZipFile(file_path, "r") as zip_ref:
+            zip_ref.extract(csv_file,folder_path)
+        os.remove(file_path)
 
 
 if __name__ == "__main__":
