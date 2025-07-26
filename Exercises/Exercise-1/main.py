ecd7b9cc3ce94e7226ad5fb7a78d282b3f0e7bfe
FCardellino <fndcardellino@gmail.com>
2025-07-26 18:56:47 -0300

    asyn downloads

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -3,6 +3,9 @@ import sys
 from pathlib import Path
 import zipfile 
 import os
+import aiohttp
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -14,13 +17,14 @@ download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
 ]
 
+cwd = Path.cwd()
+folder_path = cwd / "downloads" 
+folder_path.mkdir(parents=True,exist_ok=True)
 
-def main():
-    # your code here
-    #pass
-    cwd = Path.cwd()
-    folder_path = cwd / "downloads" 
-    folder_path.mkdir(parents=True,exist_ok=True)
+def sync_download():
+    # cwd = Path.cwd()
+    # folder_path = cwd / "downloads" 
+    # folder_path.mkdir(parents=True,exist_ok=True)
 
     for uri in download_uris:
        
@@ -35,6 +39,46 @@ def main():
             zip_ref.extract(csv_file,folder_path)
         os.remove(file_path)
 
+def save_unzip_clean(file_path,csv_file, data):
+    with open(file_path, 'wb') as f:
+        f.write(data)
+    with zipfile.ZipFile(file_path, "r") as zip_ref:
+        zip_ref.extract(csv_file,folder_path)
+    os.remove(file_path)
+
+async def async_download(session,executor,uri):
+
+    file_name = uri.split('/')[-1]
+    file_path = folder_path / file_name
+    csv_file = file_name.split(".")[0] + ".csv"
+
+    #async with aiohttp.ClientSession() as session:
+    async with session.get(uri) as response:
+        if response.status==200:
+            data = await response.read()
+            #save_unzip_clean(file_path,csv_file, data)
+            loop = asyncio.get_running_loop()
+            await loop.run_in_executor(executor, save_unzip_clean,file_path,csv_file, data)
+            
+            # with open(file_path, 'wb') as f:
+            #     f.write(data)
+            # with zipfile.ZipFile(file_path, "r") as zip_ref:
+            #     zip_ref.extract(csv_file,folder_path)
+            # os.remove(file_path)
+    
+
+async def main():
+    # your code here
+    #pass
+    #sync_download()
+    executor = ThreadPoolExecutor(max_workers=5)
+    async with aiohttp.ClientSession() as session:
+        tasks= [async_download(session,executor,uri) for uri in download_uris]
+        await asyncio.gather(*tasks)
+    executor.shutdown(wait=True)
+
+
 
 if __name__ == "__main__":
-    main()
+    #sinc_main()
+    asyncio.run(main())
