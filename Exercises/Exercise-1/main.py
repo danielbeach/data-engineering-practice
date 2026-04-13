03e3415cf15bdca8906cfd9140f7cfc78daee572
muskan123998 <32158510+muskan123998@users.noreply.github.com>
2026-04-13 23:47:11 +0530

    Update main.py

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,6 @@
 import requests
+import os
+import zipfile
 
 download_uris = [
     "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
@@ -12,7 +14,11 @@ download_uris = [
 
 
 def main():
-    # your code here
+    with requests.get(url, stream=True) as r:
+    r.raise_for_status()
+    with open(zip_path, "wb") as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            f.write(chunk)
     pass
 
 
